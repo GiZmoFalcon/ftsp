d634a33c60bb03a46e1be594047f164f18a2f4e5
Monish S R <srmonish96@gmail.com>
2018-10-29 01:56:49 +0530

    Added code for reference broadcast synchronisation

---
+++ mote.py
@@ -0,0 +1,109 @@
+from timerTest import Tissot
+import socket
+from threading import Thread
+from collections import defaultdict
+from datetime import datetime
+import sys
+import re
+from subprocess import check_output
+
+
+def time_difference(time_1, time_2):    # To subtract 2 timestamps and convert them to milliseconds
+    return (time_1[0] - time_2[0]) * 3600 * 1000 + (time_1[1] - time_2[1]) * 60000 + (time_1[2] - time_2[2]) * 1000 \
+           + time_1[3] - time_2[3]
+
+
+def millisec2stamp(ms):     # To convert milliseconds to [hh, mm, ss, ms]
+    return list(map(int, datetime.fromtimestamp(ms/1000).strftime("%H %M %S %f").split(" ")))
+
+
+def get_ips():
+    try:
+        import netifaces
+        wireless=re.findall(r'[w]\w+', str(netifaces.interfaces()))
+        abc = netifaces.ifaddresses(wireless[0])
+        ip_addr=abc[netifaces.AF_INET][0]['addr']
+        netmask=abc[netifaces.AF_INET][0]['netmask']
+        bcast_ip=abc[netifaces.AF_INET][0]['broadcast']
+        return ip_addr, bcast_ip
+    except Exception:
+        text = str(check_output(['ifconfig']))
+        m = re.findall(r'[w]\w+', text)
+        ips = re.findall(r"\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}", str(check_output(['ifconfig', m[0]])))
+        return ips[0], ips[1]
+
+
+class Mote:
+    def __init__(self, name):
+        self.name = name
+        self.host, self.bcast_ip = get_ips()
+        self.bcast_soc = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.bcast_soc.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+        self.bcast_soc.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.bcast_soc.bind(('', 5000))
+        self.conn_soc = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.conn_soc.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.conn_soc.bind(('', 5000))
+        self.server_address = ""
+        self.timer = None
+        self.pr_bcast_time = None
+        self.beacon_count = 0
+        self.offset = defaultdict(int)
+        self.timer_started = False
+
+    def broadcast(self, message):
+        self.bcast_soc.sendto(message.encode(), (self.bcast_ip, 5000))
+
+    def bcast_receive(self):
+        try:
+            while True:
+                if self.timer_started:
+                    print(self.timer.check_time(), end="\r")
+                    sys.stdout.write("\033[K")
+                msg, addr = self.bcast_soc.recvfrom(1024)
+                msg = msg.decode()
+
+                if "Server" in msg:     # Discovery Reply
+                    self.conn_soc.sendto(self.name.encode(), (addr[0], 5000))
+                    self.server_address = addr[0]
+                    print("Sending name to server with address {}".format(addr[0]))
+
+                elif ":" in msg and "TimeSync" in msg:      # Timestamp sent by server
+                    msg = list(map(int, msg.rstrip()[0].split(":")))
+                    self.timer = Tissot(msg[0], msg[1], msg[2], msg[3])
+                    Thread(target=self.timer.start).start()
+                    self.timer_started = True
+                    print("Timer started".format(self.beacon_count), end="\r")
+                    sys.stdout.write("\033[K")
+
+                elif "beacon" in msg:       # Beacon sent by server
+                    self.beacon_count += 1
+                    self.pr_bcast_time = self.timer.store_time()
+                    self.broadcast(":".join(list(map(str, self.pr_bcast_time))))    # Broadcasting beacon received time
+                    print("Beacon no. {} received".format(self.beacon_count))
+
+                elif addr[0] != self.host:  # Broadcast received from another mote
+                    time2 = list(map(int, msg.split(":")))
+                    time_diff = time_difference(self.pr_bcast_time, time2) / 2  # Halved to reduce offset variation
+                    self.offset[addr[0]] = ((self.beacon_count - 1) * self.offset[addr[0]] + time_diff)\
+                                           / self.beacon_count  # Offset calculation formula
+
+                    if self.offset[addr[0]] < 0:
+                        i = -1
+
+                    else:
+                        i = 1
+
+                    time2 = millisec2stamp(abs(self.offset[addr[0]]))
+                    present_time = [k+i*time2[j] for j, k in enumerate(self.timer.store_time())]
+                    # Updating timer
+                    self.timer.update_time(present_time[0], present_time[1], present_time[2], present_time[3])
+                    print("Timer updated from address {}".format(addr[0]), end="\r")
+                    sys.stdout.write("\033[K")
+
+        except KeyboardInterrupt:
+            return
+
+if __name__ == '__main__':
+    mote = Mote(input("Enter client's name:"))
+    mote.bcast_receive()
---
+++ server.py
@@ -0,0 +1,83 @@
+import socket
+from timerTest import Tissot
+from time import sleep
+from random import randint
+import re
+from threading import Thread
+from subprocess import check_output
+
+
+def get_ips():
+    try:
+        import netifaces
+        wireless=re.findall(r'[w]\w+', str(netifaces.interfaces()))
+        abc = netifaces.ifaddresses(wireless[0])
+        ip_addr=abc[netifaces.AF_INET][0]['addr']
+        netmask=abc[netifaces.AF_INET][0]['netmask']
+        bcast_ip=abc[netifaces.AF_INET][0]['broadcast']
+        return ip_addr, bcast_ip
+    except Exception:
+        text = str(check_output(['ifconfig']))
+        m = re.findall(r'[w]\w+', text)
+        ips = re.findall(r"\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}", str(check_output(['ifconfig', m[0]])))
+        return ips[0], ips[1]
+
+
+class Server:
+    def __init__(self):
+        self.timer = Tissot()
+        self.beacon_count = 0
+        self.name = "Server"
+        self.host, self.bcast_ip = get_ips()
+        self.bcast_soc = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.bcast_soc.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+        self.bcast_soc.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.bcast_soc.bind(('', 5000))
+        self.conn_soc = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.conn_soc.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.conn_soc.bind(('', 5000))
+        self.clients_list = {}
+        Thread(target=self.timer.start).start()
+
+    def broadcast(self, message):
+        self.bcast_soc.sendto(message.encode(), (self.bcast_ip, 5000))
+
+    def discover(self):
+        print("Discovering...")
+        try:
+            self.broadcast("PING from server")
+            while True:
+                msg, addr = self.conn_soc.recvfrom(1024)
+                if addr[0] != self.host:
+                    self.clients_list[msg.decode()] = addr[0]
+                    print("Client {0} with address {1} added to list".format(msg.decode(), addr[0]))
+        except KeyboardInterrupt:
+            return
+
+    def send_time(self):
+        print("Sending time to receivers...")
+        self.broadcast(self.timer.check_time() + "\nTimeSync")
+
+    def beacon(self):
+        print("Sending beacons\n")
+        b = 0
+        try:
+            while True:
+                b += 1
+                self.broadcast("beacon")
+                sleep(randint(2, 10))
+                print("Beacon number {} sent".format(b))
+        except KeyboardInterrupt:
+            print("Number of beacons sent: {}".format(b))
+
+    def __del__(self):
+        self.bcast_soc.close()
+        self.conn_soc.close()
+
+if __name__ == '__main__':
+    server = Server()
+    server.discover()
+    server.beacon()
+    print("Client\tAddress")
+    [print("{0}\t{1}".format(key, value)) for key, value in server.clients_list.items()]
+
