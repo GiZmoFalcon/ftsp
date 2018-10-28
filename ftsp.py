048befcb1d20beaf5feaf528a1db4628bf9e2b6a
Sourabh Daroji <darojisourabh@ymail.com>
2018-10-28 19:59:22 +0530

    fixed ip address resolution

--- ftsp.py
+++ ftsp.py
@@ -8,10 +8,20 @@ from subprocess import check_output
 
 
 def get_ips():
-    text = str(check_output(['ifconfig']))
-    m = re.findall(r'[w]\w+', text)
-    ips = re.findall(r"\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}", str(check_output(['ifconfig', m[0]])))
-    return ips[0], ips[2]
+    try:
+        import netifaces
+        wireless=re.findall(r'[w]\w+', str(netifaces.interfaces()))
+        abc = netifaces.ifaddresses(wireless[0])
+        ip_addr=abc[netifaces.AF_INET][0]['addr']
+        netmask=abc[netifaces.AF_INET][0]['netmask']
+        bcast_ip=abc[netifaces.AF_INET][0]['broadcast']
+        print(ip_addr+"\n"+bcast_ip+"\n"+netmask)
+        return ip_addr, bcast_ip
+    except Exception:
+        text = str(check_output(['ifconfig']))
+        m = re.findall(r'[w]\w+', text)
+        ips = re.findall(r"\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}", str(check_output(['ifconfig', m[0]])))
+        return ips[0], ips[1]
 
 
 class ftsp_client:
---
+++ get_ip.py
@@ -0,0 +1,15 @@
+try:
+	import re
+	import netifaces
+	wireless=re.findall(r'[w]\w+', str(netifaces.interfaces()))
+	abc = netifaces.ifaddresses(wireless[0])
+	ip_addr=abc[netifaces.AF_INET][0]['addr']
+	netmask=abc[netifaces.AF_INET][0]['netmask']
+	bcast_ip=abc[netifaces.AF_INET][0]['broadcast']
+	print(ip_addr+"\n"+bcast_ip+"\n"+netmask)
+	return ip_addr, bcast_ip
+except Exception:
+	text = str(check_output(['ifconfig']))
+    m = re.findall(r'[w]\w+', text)
+    ips = re.findall(r"\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}", str(check_output(['ifconfig', m[0]])))
+    return ips[0], ips[1]
